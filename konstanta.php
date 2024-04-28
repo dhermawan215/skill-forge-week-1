b6c913a29378ad9ab8030bf54c12f5a133c5602c
dhermawan215 <dhermawan215@gmail.com>
2024-04-28 12:56:55 +0700

    projek pertama

---
+++ konstanta.php
@@ -0,0 +1,39 @@
+<?php
+
+//php konstanta
+// define
+define('panjang', 10);
+
+// echo "nilai konstanta panjang : " . panjang;
+
+// konstanta dengan kata const
+
+const nilaiPanjang = 5;
+
+// echo "nilai konstanta panjang: " . nilaiPanjang;
+
+/**
+ * fungsi untuk menarik data mahasiswa dari tabel mahasiswa
+ */
+// function getMahasiswa(){
+
+// }
+
+// var_dump atau debuging hasil
+
+$hasil = '10';
+// echo $hasil . "<br>";
+
+// var_dump($hasil);
+//variable php
+$variabel_string = '10';
+$variabel_integer = 10;
+$variabel_decimal = 10.2;
+$variabel_bool = true;
+$variabel_null = null;
+
+var_dump($variabel_string);
+var_dump($variabel_integer);
+var_dump($variabel_decimal);
+var_dump($variabel_bool);
+var_dump($variabel_null);
---
+++ operator.php
@@ -0,0 +1,28 @@
+<?php
+
+// assignment operator (=) atau sama dengan
+
+$angka1 = 10;
+$angka2 = '10';
+
+// if ($angka1 == $angka2) {
+//     echo true;
+// } else {
+//     echo false;
+// }
+
+
+// aritmatika (+ - / *)
+// penjumlahan
+$harga1 = 11;
+
+$harga2 = 5;
+
+// echo $totalHarga = $harga1 + $harga2;
+
+//pengurangan
+// echo "artimatika pengurangan: " . $totalHargaJikaDikurangi = $harga1 - $harga2;
+//perkalian
+// echo "artimatika perkalian: " . $hargaJikaDikali = $harga1 * $harga2;
+//pembagian
+echo "artimatika pembagian: " . $hargaJikaDikali = $harga1 / $harga2;
